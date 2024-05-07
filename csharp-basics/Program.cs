090078f2992c5aedd762a62ec9430cbb816d418b
jebasingh-joshua <jeb@friendlycares.com>
2024-05-07 17:55:25 +0530

    parson A changes

--- csharp-basics/Program.cs
+++ csharp-basics/Program.cs
@@ -6,7 +6,7 @@ namespace csharp_basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the world of .Net Test");
+            Console.WriteLine("Welcome to the world of .Net Test changes by person A");
         }
     }
 }
