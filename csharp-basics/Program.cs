d00bea556fa26233fe109e1100c1707a6b899d9d
jebasingh-joshua <jeb@friendlycares.com>
2024-05-07 17:49:04 +0530

    test added

--- csharp-basics/Program.cs
+++ csharp-basics/Program.cs
@@ -6,7 +6,7 @@ namespace csharp_basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the world of .Net");
+            Console.WriteLine("Welcome to the world of .Net Test");
         }
     }
 }
